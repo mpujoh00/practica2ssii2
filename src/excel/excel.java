d9e909c899ccee04a82957d40b906d9a3ba8301c
Micaela <mpujoh00@estudiantes.unileon.es>
2020-04-04 00:52:18 +0200

    algoritmo validacion nif

---
+++ src/excel/excel.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excel;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Iterator;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+/**
+ *
+ * @author maybeitsmica
+ */
+public class excel {
+    
+    String rutaArchivo = "/resources/SistemasInformacionII.xlsx";
+    XSSFWorkbook excel;
+    
+    public excel(){
+        
+        try{ // leo el archivo excel
+            FileInputStream archivo = new FileInputStream(new File(rutaArchivo));
+            excel = new XSSFWorkbook(archivo);
+               
+        }
+        catch(Exception e){}
+    }
+    
+    public void updateCategorias(){
+        
+        XSSFSheet hoja = excel.getSheetAt(1);
+        
+        Iterator<Row> iteradorFilas = hoja.iterator();
+        
+        Row fila;
+        
+        iteradorFilas.next(); // empieza en la segunda fila
+        
+        while(iteradorFilas.hasNext()){
+            
+            fila = iteradorFilas.next();
+        }
+    }
+}
